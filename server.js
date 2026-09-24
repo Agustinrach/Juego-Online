d47d693013f563f6fd3bf7ce75317a072684bff1
Agustinrach <74275614+Agustinrach@users.noreply.github.com>
2026-09-24 18:24:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -160,8 +160,8 @@ function enviarJugadores(codigo) {
 }
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
